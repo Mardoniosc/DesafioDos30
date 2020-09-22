70bb1e4b05df06560f235518d30de45139b02c26
Mardoniosc <mardonio@live.com>
2020-09-22 10:01:56 -0300

    Criado o menu principal

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Desafio Dos 30</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="corpoPrincipal">
+    <div class="menuPrincipal">
+      <h2>Desafio dos 30</h2>
+      <br>
+      <nav>
+        <ul class="menu">
+          <li> <a href="/desafio01">Desafio 01</a></li>
+          <li> <a href="/desafio02">Desafio 02</a></li>
+          <li> <a href="/desafio03">Desafio 03</a></li>
+          <li> <a href="/desafio04">Desafio 04</a></li>
+          <li> <a href="/desafio05">Desafio 05</a></li>
+        </ul>
+      </nav>
+    </div>
+
+  </div>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,1 @@
+# DesafioDos30 API
